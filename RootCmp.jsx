5c4254d4ec3553f9cf7dc72d48a1fe01f53e35e7
Lioz Fired <friedlioz@gmail.com>
2025-06-19 14:29:57 +0300

    Add routing and create NotFound page

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,20 +4,39 @@ import { AppHeader } from "./cmps/AppHeader.jsx"
 import { HomePage } from "./pages/HomePage.jsx"
 import { About } from "./pages/About.jsx"
 import { BookIndex } from "./pages/BookIndex.jsx"
+import { BookDetails } from "./pages/BookDetails.jsx"
+import { NotFound } from "./cmps/NotFound.jsx"
+
+const Router = ReactRouterDOM.HashRouter
+const { Routes, Route, Navigate } = ReactRouterDOM
 
 export function RootCmp() {
 
-    const [page, setPage] = useState('home')
+    // const [page, setPage] = useState('home')
 
     return (
-        <section className="app">
-            <AppHeader onSetPage={(page) => setPage(page)} />
+        <Router>
+            <section className="app">
+                <AppHeader />
+                {/* <AppHeader onSetPage={(page) => setPage(page)} /> */}
 
-            <main>
-                {page === 'home' && <HomePage />}
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/home" />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/books" element={<BookIndex />} />
+
+                        <Route path="/books/:bookId" element={<BookDetails />} />
+
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+
+                    {/* {page === 'home' && <HomePage />}
                 {page === 'about' && <About />}
-                {page === 'books' && <BookIndex />}
-            </main>
-        </section>
+                {page === 'books' && <BookIndex />} */}
+                </main>
+            </section>
+        </Router>
     )
 }
